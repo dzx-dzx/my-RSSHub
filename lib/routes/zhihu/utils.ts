3615476aaf1e925279a84f9071bb596a4d4c6448
dzx-dzx <dzx_dzx@hotmail.com>
2024-07-25 19:09:19 +0800

    .

--- lib/routes/zhihu/utils.ts
+++ lib/routes/zhihu/utils.ts
@@ -55,14 +55,14 @@ export const processImage = (content: string) => {
     return $.html();
 };
 
-export const getSignedHeader = async (url: string, apiPath: string) => {
+export const getSignedHeader = async (url: string, apiPath: string, fromZhuanlan: boolean = false) => {
     // Because the API of zhihu.com has changed, we must use the value of `d_c0` (extracted from cookies) to calculate
     // `x-zse-96`. So first get `d_c0`, then get the actual data of a ZhiHu question. In this way, we don't need to
     // require users to set the cookie in environmental variables anymore.
 
     // fisrt: get cookie(dc_0) from zhihu.com
-    const dc0 = await cache.tryGet('zhihu:cookies:d_c0', async () => {
-        const response1 = await ofetch.raw(url);
+    const { dc0, zseCk } = await cache.tryGet('zhihu:cookies:d_c0, __zse_ck', async () => {
+        const response1 = await ofetch.raw(fromZhuanlan ? 'https://www.zhihu.com/people/diygod' : url);
         const zseCk = response1._data.match(/var e="__zse_ck",t=\(typeof __g\.ck == 'string' && __g\.ck\)\|\|"([\w+/=]*?)",_=6048e5;/)?.[1];
 
         const response2 = zseCk
@@ -86,7 +86,7 @@ export const getSignedHeader = async (url: string, apiPath: string) => {
             throw new Error('Failed to extract `d_c0` from cookies');
         }
 
-        return dc0;
+        return { dc0, zseCk };
     });
 
     // calculate x-zse-96, refer to https://github.com/srx-2000/spider_collection/issues/18
@@ -94,7 +94,7 @@ export const getSignedHeader = async (url: string, apiPath: string) => {
     const f = `${xzse93}+${apiPath}+${dc0}`;
     const xzse96 = '2.0_' + g_encrypt(md5(f));
     return {
-        cookie: `d_c0=${dc0}`,
+        cookie: `d_c0=${dc0}`.concat(fromZhuanlan && zseCk ? `; __zse_ck=${zseCk}` : ''),
         'x-zse-96': xzse96,
         'x-app-za': 'OS=Web',
         'x-zse-93': xzse93,
